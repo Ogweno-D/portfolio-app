4421d660ce09a4898a0647e66e53579c13fbfdfb
Ogweno-D <dennisogwene@gmail.com>
2023-09-25 17:58:48 +0300

    Logo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,16 @@ const About = () => {
             {/* Container */}
             <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                 <div className='sm:text-right pb-8 pl-4'>
-                <p className='text-4xl font-bold inline border-b4 border-pink-600'>About</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+                    About
+                </p>
                 </div>
                 <div>  
                 </div>
 
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
-                        <p> Hi, I'm Dennis, nice to meet you. Please  take a look around </p>
+                        <p> Hi, I'm Dennis Ogweno, nice to meet you. Please  take a look around </p>
                     </div>
                     <div>
                         <p>
